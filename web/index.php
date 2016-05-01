e32f1f3f3ef1227b3acf46eafe6802d12a63f440
栗田昌平 <SHOHEIKURITA@kuritashouhei-no-MacBook-Pro.local>
2016-05-01 17:43:04 +0900

    FirstCommit

--- web/index.php
+++ web/index.php
@@ -1,37 +1,102 @@
 <?php
-$access_token = "[ACCESS TOKEN]";
 
-// メッセージ受信
-$json_string = file_get_contents('php://input');
-$json_object = json_decode($json_string);
-$from_user_id =$json_object->entry{0}->messaging{0}->sender->id;
+require('../vendor/autoload.php');
 
-$post = <<< EOM
-{
-    "recipient":{
-        "id":"{$from_user_id}"
-    },
-    "message":{
-        "text":"hello, world!"
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+
+$app->before(function (Request $request) use($bot) {
+    // TODO validation
+});
+
+$app->get('/callback', function (Request $request) use ($app) {
+    $response = "";
+    if ($request->query->get('hub_verify_token') === getenv('FACEBOOK_PAGE_VERIFY_TOKEN')) {
+        $response = $request->query->get('hub_challenge');
     }
-}
-EOM;
 
-// メッセージ送信
-api_send_request($access_token, $post);
+    return $response;
+});
 
-function api_send_request($access_token, $post) {
-    error_log("api_get_message_content_request start");
-    $url = "https://graph.facebook.com/v2.6/me/messages?access_token={$access_token}";
-    $headers = array(
-        "Content-Type: application/json"
-    );
+$app->post('/callback', function (Request $request) use ($app) {
+    // Let's hack from here!
+    $body = json_decode($request->getContent(), true);
+    $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    $output = curl_exec($curl);
-}
+    foreach ($body['entry'] as $obj) {
+        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
+
+        foreach ($obj['messaging'] as $m) {
+            $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
+            $from = $m['sender']['id'];
+            $text = $m['message']['text'];
+
+            switch ($text) {
+
+                case 'あ':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('あ', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'い':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('い', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'う':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('う', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                default:
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('%s、ちゃうねん！', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+
+            }
+        }
+
+    }
+
+    return 0;
+});
+
+$app->run();
