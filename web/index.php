5b2de2c32f29a29588cc6a777e5463beffec6b3e
栗田昌平 <SHOHEIKURITA@kuritashouhei-no-MacBook-Pro.local>
2016-05-04 10:05:15 +0900

    some

--- web/index.php
+++ web/index.php
@@ -61,11 +61,12 @@ $app->post('/callback', function (Request $request) use ($app) {
                         ],
                         'message' => [
                             'text' => sprintf('い', $text), 
-			     'attachment':{
-				    "type":"image",
-				    "payload":{
-					"url":"https://adachiroid.herokuapp.com/images/13090170_904137979695719_768070574_n.jpg"
+			     'attachment' : {
+				    "type" :"image",
+				    "payload" : {
+					"url" :kj"https://adachiroid.herokuapp.com/images/13090170_904137979695719_768070574_n.jpg"
 				    }
+				}
                         ],
                     ];
                     $client->request('POST', $path, ['json' => $json]);
