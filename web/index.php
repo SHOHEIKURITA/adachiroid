bbeb4cda2777e0552c67aabd6ef29ea8d3ee3f45
栗田昌平 <SHOHEIKURITA@kuritashouhei-no-MacBook-Pro.local>
2016-05-06 16:04:45 +0900

    FirstCommit

--- web/index.php
+++ web/index.php
@@ -47,13 +47,179 @@ $app->post('/callback', function (Request $request) use ($app) {
                             'id' => $from, 
                         ],
                         'message' => [
-                            'text' => sprintf('ハロー', $text), 
+                            'text' => sprintf('よほほー', $text), 
                         ],
                     ];
                     $client->request('POST', $path, ['json' => $json]);
                 break;
 
-                case '元気？':
+                case '元気？':case '調子どう？':case '大丈夫？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('鼻がズビズビ', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'ごめん':case 'ごめんね':case 'ごめんなさい':case 'すみません':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('慰謝料としてステーキを要求する', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'あそぼ':case '遊ぼう':case 'あそぼう':case 'どっかいこ':case 'どっか行こう':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('銭湯行って参ります', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case '何それ？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('松ぼっくりを拾いました', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'がんばったね':case '頑張ったね':case 'がんばった':case '頑張った':case 'おめでとう':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('それにしても、お腹が減りました', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case '学長ってどんな人？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('髪の分け目変えなきゃハゲるよ', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case '笹野さんってどんな人？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('お兄ちゃんみないな人', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case '塩谷先生ってどんな人？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('目力強いから気をつけて', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case '和田さんってどんな人？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('お姉さんみたいな人', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'じゃあね':case 'ばいばい':case 'バイバイ':case 'さよなら':case 'さようなら':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('みなさま良い1日を', $text), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'ひま':case '暇':case '何してる？':case '何してるの？':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'attachment' => [
+                                  'type' => 'image',
+                                  'payload' => [
+                                      'url' => 'https://adachiroid.herokuapp.com/images/01.jpg',
+                                  ],
+                              ],
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'ありがとう':case 'ありがと':case '感謝':case 'サンキュー':case 'サンキュ':
+                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                    $json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'attachment' => [
+                                  'type' => 'image',
+                                  'payload' => [
+                                      'url' => 'https://adachiroid.herokuapp.com/images/13082079_904138323029018_928492546_n.jpg',
+                                  ],
+                              ],
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $json]);
+                break;
+
+                case 'かわいいね':case 'かわいい':case 'カワイイ':case '可愛い':case 'すきだよ':case '好きだよ':
                     $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
                     $json = [
                         'recipient' => [
@@ -66,19 +232,6 @@ $app->post('/callback', function (Request $request) use ($app) {
                                       'url' => 'https://adachiroid.herokuapp.com/images/13125193_907149892727861_1078452890_n.gif',
                                   ],
                               ],
-                        ],
-                    ];
-                    $client->request('POST', $path, ['json' => $json]);
-                break;
-
-                case strpos($subject,'？'):
-                    $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
-                    $json = [
-                        'recipient' => [
-                            'id' => $from, 
-                        ],
-                        'message' => [
-                            'text' => sprintf('う', $text), 
                         ],
                     ];
                     $client->request('POST', $path, ['json' => $json]);
